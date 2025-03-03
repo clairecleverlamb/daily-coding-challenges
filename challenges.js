9314d0ed0ec632c6c97390bfb043d30d2735892e
clairecleverlamb <clairechenszeying@gmail.com>
2025-03-02 23:56:05 -0800

    finished 17

--- challenges.js
+++ challenges.js
@@ -755,8 +755,17 @@ mapArray( ['rose', 'tulip', 'daisy'], function(f, i) {
 -----------------------------------------------------------------------------*/
 // Your solution for 17-mapArray here:
 
+function mapArray(arr, callback){
+  let result = [];
+  for(let i = 0; i < arr.length; i++){
+    result.push(callback(arr[i], i));
+  }
+  return result;
+}
 
-
+console.log(mapArray( [1, 2, 3], function(n) {
+  return n * 3
+} ))
 
 
 /*-----------------------------------------------------------------------------
